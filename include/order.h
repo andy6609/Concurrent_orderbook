9acd1333ca9115ec3fa2e501f776ccb1fb0ee9fd
이동윤 <andy.lee.madeinseoul@gmail.com>
2026-03-31 10:15:00 +0900

    Add Trade struct for execution event recording

--- include/order.h
+++ include/order.h
@@ -35,6 +35,15 @@ struct Order {
     bool is_filled() const { return remaining == 0; }
 };
 
+struct Trade {
+    uint64_t trade_id;
+    uint64_t buy_order_id;
+    uint64_t sell_order_id;
+    uint32_t symbol_id;
+    uint64_t price;
+    uint64_t quantity;
+};
+
 inline std::ostream& operator<<(std::ostream& os, const Order& o) {
     os << "Order{id=" << o.id 
        << " " << (o.side == Side::BUY ? "BUY" : "SELL")
